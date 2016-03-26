13f83efafbe5071f576644371e83972d0ec490a0
Volodymyr Kuznetsov <ks.vladimir@gmail.com>
2016-03-25 17:10:55 -0700

    Make sure NativeMessagingHosts is created during installation

--- host/install.sh
+++ host/install.sh
@@ -12,6 +12,7 @@ CHROME_LIB="$HOME/Library/Application Support/Google/Chrome"
 
 echo "Configuring chrome to use cpass.py at $CPASS_NATIVE_APP..."
 
+mkdir -p "$CHROME_LIB/NativeMessagingHosts"
 sed -e "s|CPASS_EXTENSION_ID|$CPASS_EXTENSION_ID|" -e "s|CPASS_NATIVE_APP|$CPASS_NATIVE_APP|" \
     $CPASS_DIR/com.ksvladimir.cpass.json > "$CHROME_LIB/NativeMessagingHosts/com.ksvladimir.cpass.json"
 
